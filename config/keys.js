6be7f597778e7050a4dd49b09f10cb5e75e60136
AshrafZohdi <mohammadashraf2001@gmail.com>
2022-06-16 08:06:34 +0100

    created server.js

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://Main_User:Batooljana2001@cluster0.atasl.mongodb.net/test"
+};
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// bodyParser middleware
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// connect to MongoDB
+mongoose
+    .connect(
+    db,
+    {useNewUrlParser: true}
+)
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log("err"));
+
+
+// process.env.port is Heroku's port
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
