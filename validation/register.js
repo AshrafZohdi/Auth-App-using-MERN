2ff328ef5cc63d5f29896a5354b6e32b62909e1b
AshrafZohdi <mohammadashraf2001@gmail.com>
2022-06-19 14:35:29 +0100

    finished registration validation

--- validation/register.js
+++ validation/register.js
@@ -22,4 +22,24 @@ module.exports = function validateRegisterInput(data) {
     else if (!validator.isEmail(data.email)) {
         errors.email = "Email is invalid";
     }
-}
+
+    // Password Checks
+    if (validator.isEmpty(data.password)) {
+        errors.password = "Password field required";
+    }
+    if (validator.isEmpty(data.password2d2)) {
+        errors.password2 = "Confirm Password is required";
+    }
+
+    if (!validator.isLength(data.password, { min:6, max:30})) {
+        errors.password = "Password must be at leat 6 characters";
+    }
+    if (!validator.equals(data.password, data.password2)) {
+        errors.password = "Passwords are not matching";
+    }
+
+    return{
+        errors,
+        isValid: isEmpty(errors)
+    };
+};
