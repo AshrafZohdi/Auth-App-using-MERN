35aaaaceb0e2ce09c9ff0d8cf9c804f82efee124
AshrafZohdi <mohammadashraf2001@gmail.com>
2022-06-23 14:06:15 +0100

    Update register.js

--- validation/register.js
+++ validation/register.js
@@ -1,45 +1,37 @@
-const isEmpty = require('is-empty');
-let validator = require('validator');
-
+const Validator = require("validator");
+const isEmpty = require("is-empty");
 module.exports = function validateRegisterInput(data) {
-    let errors = {};
-
-    // Convert empty fields to an empty string so we can use validator functions
-    data.name = !isEmpty? data.name: "";
-    data.email = !isEmpty? data.email: "";
-    data.password = !isEmpty? data.password: "";
-    data.password2 = !isEmpty? data.password2: "";
-
-    // Name Check
-    if (validator.isEmpty(data.name)) {
-        errors.name = "Name field is required";
-    }
-
-    // Email Checks
-    if (validator.isEmpty(data.email)) {
-        errors.email = "Email field is required";
-    }
-    else if (!validator.isEmail(data.email)) {
-        errors.email = "Email is invalid";
-    }
-
-    // Password Checks
-    if (validator.isEmpty(data.password)) {
-        errors.password = "Password field required";
-    }
-    if (validator.isEmpty(data.password2)) {
-        errors.password2 = "Confirm Password is required";
-    }
-
-    if (!validator.isLength(data.password, { min:6, max:30})) {
-        errors.password = "Password must be at leat 6 characters";
-    }
-    if (!validator.equals(data.password, data.password2)) {
-        errors.password = "Passwords are not matching";
-    }
-
-    return{
-        errors,
-        isValid: isEmpty(errors)
-    };
+  let errors = {};
+// Convert empty fields to an empty string so we can use validator functions
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+// Name checks
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
+// Email checks
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  } else if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+// Password checks
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required";
+  }
+if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
+  }
+return {
+    errors,
+    isValid: isEmpty(errors)
+  };
 };
