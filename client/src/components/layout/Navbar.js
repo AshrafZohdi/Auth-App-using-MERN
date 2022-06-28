c4a7f4397d4c506e728dba754c1ec3330d2ee0e9
AshrafZohdi <mohammadashraf2001@gmail.com>
2022-06-28 08:27:52 +0100

    started working on navbar

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div className="navbar-fixed">
+            <nav className="z-depth-0">
+                <div className="nav-wrapper-white">
+                    
+                </div>
+            </nav>
+        </div>
+    )
+}
