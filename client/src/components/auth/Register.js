5eae2845ef071b3f9d6e569b15ace6e183a96194
AshrafZohdi <mohammadashraf2001@gmail.com>
2022-08-03 21:20:22 +0100

    Update Register.js
    Submit button added

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -90,15 +90,26 @@ class Register extends component {
                                 />
                                 <label htmlFor="password">Confirm Password</label>
                             </div>
-
+                            <div className="col s12" style={{paddingLeft:"11.250px"}}>
+                                <button
+                                    style={{
+                                        width: "150px",
+                                        borderRadius: "3px",
+                                        letterSpacing: "1.5px",
+                                        marginTop: "1rem"
+                                    }}
+                                    type="submit"
+                                    className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                                >
+                                    Sign up
+                                </button>
+                            </div>
                         </form>
-
                     </div>
-
                 </div>
-
             </div>
         )
     }
-
 }
+
+export default Register;
