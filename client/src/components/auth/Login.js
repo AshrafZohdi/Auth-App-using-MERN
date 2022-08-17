028bef3d9c378d99d50a7e0ebf82e4ff92c7f787
AshrafZohdi <mohammadashraf2001@gmail.com>
2022-08-17 09:55:40 +0100

    Update Login.js

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -42,7 +42,7 @@ class Login extends component {
                             </i>
                             Back to Home
                         </Link>
-                        <div className="col s12" style={{ paddingLeft: "11.25px"}}>
+                        <div className="col s12" style={{ paddingLeft: "11.250px"}}>
                             <h4>
                                 <b>Login</b> below
                             </h4>
@@ -51,11 +51,46 @@ class Login extends component {
                             </p>
                         </div>
                         <form noValidate onSubmit={this.onSubmit}>
-
+                            <div className="input-field col s12">
+                                <input
+                                    onChange={this.onChange}
+                                    value={this.state.email}
+                                    error={errors.email}
+                                    id="email"
+                                    type="email"
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field col s12">
+                                <input
+                                    onChange={this.onChange}
+                                    value={this.state.password}
+                                    error={errors.password}
+                                    id="password"
+                                    type="password"
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                            <div className="col s12" style={{ paddingLeft: "11.250px"}}>
+                                <button
+                                    style={{
+                                        width: "150px",
+                                        borderRadius: "3px",
+                                        letterSpacing: "1.5px",
+                                        marginTop: "1rem"
+                                    }}
+                                    type="submit"
+                                    className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                                >
+                                    Login
+                                </button>
+                            </div>
                         </form>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
+
+export default Login;
