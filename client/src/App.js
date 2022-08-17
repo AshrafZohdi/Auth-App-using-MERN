4ed6e5217394c105531cffaef081ebba19966734
AshrafZohdi <mohammadashraf2001@gmail.com>
2022-08-17 10:06:09 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register'
+import Login from './components/auth/Login';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+      </div>
+    </Router>
   );
 }
 
